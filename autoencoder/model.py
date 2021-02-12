b4b5bf1d1f2f561b065d1d79feff165901c724f4
Shauray <shauray9@gmail.com>
2021-02-12 12:46:44 +0530

    maybe autoencoder can do this

---
+++ autoencoder/model.py
@@ -0,0 +1,28 @@
+import torch
+import math
+import numpy
+import torch.nn as nn
+
+class encoder(nn.Module):
+    def __init__(self):
+        super(encoder, self).__init__()
+        pass
+    def forward(self, x):
+        pass
+
+class decoder(nn.Module):
+    def __init__(self):
+        super(decoder, self).__init__()
+        pass
+
+
+    def forward(self, x):
+        pass
+
+EPOCH = 50
+
+for epoch in range(EPOCH):
+    pass
+
+
+# the basic structure for the autoencoder 
