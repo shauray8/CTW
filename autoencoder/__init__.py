7130049053808d04e5745e88c084322b31cfa656
Shauray <shauray9@gmail.com>
2021-02-12 12:52:04 +0530

    resnet to the win (maybe)

---
+++ autoencoder/__init__.py
@@ -0,0 +1,6 @@
+'''trying to compress things with an autoencoder i know it us a lossy compression 
+ but i dont think it matters when it comes to images !!'''
+
+ class Node():
+     def __init__(self):
+         pass
