f309438ba8b2e996cabf16ed4b43b3da26e1a546
Shauray <shauray9@gmail.com>
2021-02-12 12:49:19 +0530

    resnet might die

--- autoencoder/model.py
+++ autoencoder/model.py
@@ -19,6 +19,15 @@ class decoder(nn.Module):
     def forward(self, x):
         pass
 
+class resnet(nn.Module):
+    def __init__(self):
+        super(resnet).__init__()
+        pass
+    def forward(self, x):
+        pass
+
+    #because i think that i will need one lets see
+
 EPOCH = 50
 
 for epoch in range(EPOCH):
