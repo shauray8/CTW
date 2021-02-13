7cce07497bd5c9e89bbafecc07305b568e5320ce
Shauray <shauray9@gmail.com>
2021-02-13 16:52:01 +0530

    implimenting resnet

--- autoencoder/model.py
+++ autoencoder/model.py
@@ -4,12 +4,13 @@ import numpy
 import torch.nn as nn
 from torchvision import transforms, datasets
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 transform = transforms.Compose([
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.5],
+                             std=[0.5])
 ])
 
 batch_size=32
@@ -19,9 +20,9 @@ dataset_loader = torch.utils.data.DataLoader(dataset,
                                              batch_size=batch_size, shuffle=True,)
 
 class encoder(nn.Module):
-    def __init__(self):
+    def __init__(self, input, channels):
         super(encoder, self).__init__()
-        self.conv1 = nn.Conv2d(28*28,64, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(input,channels, kernel_size=5, stride=2)
         self.act = nn.ReLU()
         pass
     def forward(self, x):
@@ -54,17 +55,23 @@ class resnet(nn.Module):
     #because i think that i will need one lets see
 
 EPOCH = 50
-encoder = encoder()
+input = 1
+channels = 64
+encoder = encoder(input, channels)
 decoder = decoder()
 print(encoder)
 print(decoder)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def train():
     for epoch in range(EPOCH):
         for id, (images,_) in enumerate(dataset_loader):
-            images = images.to('cuda')
-            pass
-        pass
+            #images = images.to(device)
+            plt.imshow(images[0].view(28,28), cmap="gray")
+            plt.show()
+            print("done bitches")
+            break 
+        break
 
-
+train()
 # the basic structure for the autoencoder 
