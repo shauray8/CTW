f07f35054d6cda194a73f95fca743a7346e3e96f
Shauray <shauray9@gmail.com>
2022-07-12 08:38:34 +0530

    researching about the size difference

--- audio_compression/research.py
+++ audio_compression/research.py
@@ -1,13 +1,26 @@
 import librosa
+import librosa.display
 import numpy as np
 import argparse
+import matplotlib.pyplot as plt
 
-def mel_spec():
-    pass
+def mel_spec(data,sr):
+    ft = np.abs(librosa.stft(data, n_fft=2048,  hop_length=512))
+    librosa.display.specshow(ft, sr=sr, x_axis='time', y_axis='linear');
+    plt.colorbar();
+    ft_dB = librosa.amplitude_to_db(ft, ref=np.max)
+    librosa.display.specshow(ft_dB, sr=sr, hop_length=1024, x_axis='time', y_axis='log');
+    plt.show()
+
+def wave(audio_data:np.array) -> None:
+    librosa.display.waveplot(data,sr=sr)
+    plt.show()
 
 def main(audio_fn:str) -> any:
     data,sr = librosa.load(audio_fn, sr=None)
     print(np.array(data))
+    mel_spec(data,sr)
+
     return
 
 if __name__ == "__main__":
